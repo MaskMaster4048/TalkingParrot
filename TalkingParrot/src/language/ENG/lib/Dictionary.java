a4a3c655ee942670650c39ab10c872a7655bd7ed
MaskMaster4048 <logan104plumlee@gmail.com>
2018-04-15 11:55:54 -0500

    changed String list into a Map

--- TalkingParrot/src/language/ENG/lib/Dictionary.java
+++ TalkingParrot/src/language/ENG/lib/Dictionary.java
@@ -1,8 +1,11 @@
 package language.ENG.lib;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Dictionary {
 	private Word[] wordsFull = getWords();
-	private String[] wordString; //
+	private Map<String, Integer> wordStr = new HashMap<String, Integer>();
 	
 	private static Word[] getWords() {
 		//get all the words from a file
