e64caadf390d96f483fdc689f7c29260d6e8cf74
MaskMaster4048 <logan104plumlee@gmail.com>
2018-05-06 18:34:02 -0500

    added "see also" into javadoc

--- TalkingParrot/src/external/jsoncreator/NotAJsonFileException.java
+++ TalkingParrot/src/external/jsoncreator/NotAJsonFileException.java
@@ -4,6 +4,7 @@ package external.jsoncreator;
  * An exception thrown by the Json class if a file it is reading is not in fact a Json file.
  * @author Logan Plumlee
  * @since 2018-5-5
+ * @see Json
  */
 public class NotAJsonFileException extends Exception {
 	private static final long serialVersionUID = 1L;
